6b956f5821fd2f5dabf95a429016b4436292d161
sanjitpal123 <146546817+sanjitpal123@users.noreply.github.com>
2024-01-10 13:27:15 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,4 @@
+//Use Array.from() to create a new array where each element is the square of the corresponding element in the original array.
+const arr=[4,5,6,7];
+const squre=Array.from(arr,(a)=>a*a);
+console.log(squre);

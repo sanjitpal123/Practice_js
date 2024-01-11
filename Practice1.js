619886e7b1076fa7b2b67a24adeb6d819ced158c
sanjitpal123 <146546817+sanjitpal123@users.noreply.github.com>
2024-01-11 09:54:22 +0530

    Add files via upload

---
+++ Practice1.js
@@ -0,0 +1,6 @@
+
+/*Slice from the End:
+Given the array const colors = ["red", "green", "blue", "yellow", "purple"], use the slice method to extract the last two elements.*/
+
+const Color = ["red", "green", "blue", "yellow", "purple"];
+console.log(Color.slice(-2));

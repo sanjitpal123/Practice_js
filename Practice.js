abe47a4c0c9d49cc9bcfff5acd5c800c4a88509a
sanjitpal123 <146546817+sanjitpal123@users.noreply.github.com>
2024-01-11 09:45:37 +0530

    Add files via upload

---
+++ Practice.js
@@ -0,0 +1,3 @@
+//Given the array const numbers = [1, 2, 3, 4, 5], use the slice method to create a new array containing elements from index 1 to index 3.
+const number=[1,2,3,4,5];
+console.log(number.slice(1,3));

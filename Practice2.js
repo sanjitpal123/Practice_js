7762530c1fe9776ec5d0acefdce393084146ae41
sanjitpal123 <146546817+sanjitpal123@users.noreply.github.com>
2024-01-11 10:16:52 +0530

    Add files via upload

---
+++ Practice2.js
@@ -0,0 +1,4 @@
+//Given the array const elements = ["earth", "fire", "air", "water"], use variables to store start and end indices and create a new array using the slice method.
+const elements=["earth","fire","air","water"];
+const newelements=elements.slice(0,3);
+console.log(newelements);

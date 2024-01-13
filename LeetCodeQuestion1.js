26579819774465bdf8ec589370dd0184850ccd86
sanjitpal123 <146546817+sanjitpal123@users.noreply.github.com>
2024-01-13 18:44:01 +0530

    Add files via upload

---
+++ LeetCodeQuestion1.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+    x = x.toString(); 
+      let s = x.split('').reverse().join('');
+  
+      if (s === x) {
+          return true;
+      } else {
+          return false;
+      }
+  };

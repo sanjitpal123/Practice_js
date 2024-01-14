1bb4a4e425f1411ae9413a52bf9086c7b9fd5ee2
sanjitpal123 <146546817+sanjitpal123@users.noreply.github.com>
2024-01-14 09:26:37 +0530

    Add files via upload

---
+++ LeetCodeSolving3.js
@@ -0,0 +1,12 @@
+// Solving missing number
+
+var missingNumber = function(nums) {
+    
+  for(let i=0;i<=nums.length;i++)
+  {
+      if(!nums.includes(i))
+      {
+          return i;
+      }
+  }
+};

fb2d74cd30f8eea799e842c7c863594a17232d39
sanjitpal123 <146546817+sanjitpal123@users.noreply.github.com>
2024-01-30 14:33:25 +0530

    Add files via upload

---
+++ CatchDemo1.js
@@ -0,0 +1,21 @@
+const promse= function createpromise()
+{
+    return new Promise((res,rej)=>{
+        rej('sanjit');
+    });
+}
+const pr1=promse();
+pr1
+.then(function onfullfillment(value){
+    console.log("pr1 is fullfillment with",value);
+},function ofrejected(value){
+    console.log("pr1 is rejected with",value);
+    throw 33
+})
+.then(function onfull(value){
+    console.log("pr2 is fullfill with",value);
+} )
+.catch(function error(error){
+    console.log("error came betwen",error);
+})
+

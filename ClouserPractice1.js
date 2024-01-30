82c3826e9784e2e6944bc30bd9f05e3026a9130e
sanjitpal123 <146546817+sanjitpal123@users.noreply.github.com>
2024-01-30 15:26:40 +0530

    Add files via upload

---
+++ ClouserPractice1.js
@@ -0,0 +1,13 @@
+function ClouserDemo(task)
+{
+    let i=0;
+    function demo()
+    {
+        console.log("task is:",task);
+        i+=1;
+        console.log(i);
+    }
+    demo();
+    
+}
+ClouserDemo('do something');

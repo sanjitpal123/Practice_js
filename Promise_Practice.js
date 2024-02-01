903d5b8d1a9e854e3295633652bea5bf90d73b25
sanjitpal123 <146546817+sanjitpal123@users.noreply.github.com>
2024-02-01 11:57:04 +0530

    Add files via upload

--- Promise_Practice.js
+++ Promise_Practice.js
@@ -1,11 +1,19 @@
-const promise1= new Promise((res,rej)=> setTimeout(()=>res("sanjit"),100));
-const promise2= new Promise((res,rej)=> setTimeout(()=>rej("Manjit"),40));
-const promise3=new  Promise((res,rej)=> setTimeout(()=>res("Ranjit"),50));
-const racepromise= Promise.race([promise1,promise2,promise3]);
-racepromise.then((value)=>{
-    console.log(value,"Congratulations you won");
-},(value)=>{
-    console.log(value,"rejected");
-})
+//Write a promise that resolves successfully and use .then() to handle the success. Then, introduce an error using throw and handle it with .catch().
+const promise=new Promise((res,rej)=> res(45));
 
+promise.then((value)=>{
+    console.log("Promise one is successfull with",value);
+
+});
+const promise2=new Promise((res,rej)=> rej(6));
+promise2.then((value)=>{
+    console.log("successfull with ",value);
+
+},(value)=>{
+    throw value;
+
+}).catch((error)=>{
+    console.log("Error is :" , error);
+
+});
 

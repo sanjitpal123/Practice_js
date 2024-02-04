13aa0e6251ca680ef3251e12a8e7c732b2786366
sanjitpal123 <146546817+sanjitpal123@users.noreply.github.com>
2024-02-04 23:23:53 +0530

    Add files via upload

---
+++ Await_Output_Guessing_Qestion4.js
@@ -0,0 +1,12 @@
+async function guessOutput4() {
+  console.log("Start");
+
+  const value = await 42;
+  console.log(value);
+
+  console.log("End");
+}
+
+guessOutput4();
+
+//Output is "Start" 42 "End";

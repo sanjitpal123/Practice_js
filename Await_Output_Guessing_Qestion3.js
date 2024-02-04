90369f103babac02a209929513e63c8701e5153d
sanjitpal123 <146546817+sanjitpal123@users.noreply.github.com>
2024-02-04 23:19:07 +0530

    Add files via upload

---
+++ Await_Output_Guessing_Qestion3.js
@@ -0,0 +1,16 @@
+async function guessOutput3() {
+  console.log("Start");
+
+  const promise1 = new Promise(resolve => setTimeout(() => resolve("Promise 1"), 500));
+  const promise2 = new Promise(resolve => setTimeout(() => resolve("Promise 2"), 300));
+
+  const [result1, result2] = await Promise.all([promise1, promise2]);
+
+  console.log(result1);
+  console.log(result2);
+
+  console.log("End");
+}
+
+guessOutput3();
+// output is "Start" "Promise 1" "Promise2" "End"

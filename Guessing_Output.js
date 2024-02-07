82ed2eeaf44e7be7a8dfba848f1071313eae24fb
sanjitpal123 <146546817+sanjitpal123@users.noreply.github.com>
2024-02-07 12:50:23 +0530

    Add files via upload

---
+++ Guessing_Output.js
@@ -0,0 +1,12 @@
+const obj={
+    x:10,
+    y:20,
+    display:()=>{
+      
+        console.log(`value of x is ${x} and value of y is ${y}`);
+
+    }
+
+}
+obj.display();
+// Guess the output before executing the code , i have guessed and got the right answer

18982d12128afd3ff570b37fdd6a4b7e6105861e
sanjitpal123 <146546817+sanjitpal123@users.noreply.github.com>
2024-02-08 20:14:56 +0530

    Add files via upload

--- Promise_all_setteled_Question3.js
+++ Promise_all_setteled_Question3.js
@@ -1,44 +1,45 @@
-
+//Write a function that accepts an array of promises and a delay value. Use Promise.allSettled to wait for all promises to settle and return an array with the results, including any delayed values resolved after the specified delay.
 function promise1()
 {
     return new Promise((res,rej)=>{
         setTimeout(()=>{
-            res("success");
+            res(10); 
         },1000);
+      
     });
 }
+
 function promise2()
 {
     return new Promise((res,rej)=>{
         setTimeout(()=>{
-            res(5);
+            rej(2929);
         },2000);
-    });
-}
-function promise3()
-{
-    return new Promise((res,rej)=>{
-        setTimeout(()=>{
-            rej("rejected");
-        },3000);
-    });
-}
-
-async function Execute(arroftask){
-   try{
-    const results=await Promise.allSettled(arroftask.map(task=>task()));
-    results.forEach((result,index)=>{
-         if(result.status==="fulfilled")
-         {
-            console.log(`Promse ${index+1}: is fullfill with value:${result.value}`);
-         }
-         else{
-         console.log(`Promse ${index+1}: is rejected with value:${result.reason}`);
-         }
     })
-   }catch(error)
-   {
-    console.log(error);
-   }
 }
-Execute([promise1,promise2,promise3]);
+async function  execute(arr)
+{
+    let arrofresult=[];
+    try{
+        const results=await Promise.allSettled(arr.map((arr)=>arr()));
+         results.forEach((result,index)=>{
+            if(result.status==="fulfilled")
+            {
+             arrofresult.push(`promse ${index+1} is fullfilled with: ${result.value}`);
+            }
+            else{
+                arrofresult.push(`promse ${index+1} is rejected  with: ${result.reason}`);
+            }
+         })
+        } catch(error)
+        {
+            console.log(error);
+        }
+        return arrofresult;
+}
+const pr1=execute([promise1,promise2]);
+pr1.then((result)=>{
+    console.log(result);
+},(result)=>{
+    console.log(result);
+});

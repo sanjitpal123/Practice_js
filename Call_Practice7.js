04919da6ff1a6ab495d2c66bd10b7b2de01f9c70
sanjitpal123 <146546817+sanjitpal123@users.noreply.github.com>
2024-02-24 21:42:00 +0530

    Add files via upload

---
+++ Call_Practice7.js
@@ -0,0 +1,6 @@
+//Create an arrow function printMessage that takes a message as an argument and logs it. Use the call method to invoke printMessage with a specified context.
+ function printMessage(message)
+ {
+  console.log(message);
+ }
+ printMessage.call(null,"hello sanjit");

2554812951a4fbd505493c33c03941e492d04fef
sanjitpal123 <146546817+sanjitpal123@users.noreply.github.com>
2024-02-24 21:15:10 +0530

    Add files via upload

--- Call_Practice4.js
+++ Call_Practice4.js
@@ -1,16 +1,6 @@
-//Given two objects obj1 and obj2, each containing a greet method, use the call method to invoke the greet method of obj1 with the context of obj2.
-const obj1={
-    name:'sanjit',
-    greet:function()
-    {
-        console.log(` Hello ${this.name}`);
-    }
+//Define a function getGlobalContext that logs the value of this when invoked. Use the call method to invoke getGlobalContext without explicitly passing a context.
+function getGlobalContext()
+{
+    console.log(this);
 }
-const obj2={
-    name:'manjit',
-    greet:function()
-    {
-        console.log(`Hello ${this.name}`);
-    }
-}
-obj1.greet.call(obj2);
+getGlobalContext.call(null);
